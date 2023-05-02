34058a09e309b158511ff768530667326bbd30d6
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-05-02 12:33:42 +0300

    chore: add `head` in interface ICircleDetail

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,6 +3,8 @@ import {ElementStates} from "./element-states";
 export interface ICircleDetail {
     symbol?: null | string;
     state: ElementStates;
+    head?: string;
+    characters?: string | null;
 }
 
 export interface IColumn {
