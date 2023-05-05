ec672b7db69ea2d55294b374b040d4f1c602eb3d
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-05-05 14:37:44 +0300

    chore: correct imports

--- src/pages/sorting-page/sorting-page.tsx
+++ src/pages/sorting-page/sorting-page.tsx
@@ -10,13 +10,9 @@ import {ElementStates} from "../../types/element-states";
 import {setDelay} from "../../utils/set-delay";
 import {SHORT_DELAY_IN_MS} from "../../constants/delays";
 import {swapArrayDetails} from "../../utils/swap-array-details";
-import {array} from "./get-array";
+import {array} from "./utils/get-array";
+import {TSortingPageState} from "../../types/t-sorting-page-state";
 
-type TSortingPageState = {
-    isIncreasing: boolean;
-    isDecreasing: boolean;
-    isLoading: boolean;
-}
 export const SortingPage: React.FC = () => {
 
     const [initialArray, setInitialArray] = useState<Array<IColumn>>([])
@@ -60,63 +56,53 @@ export const SortingPage: React.FC = () => {
         initialArraySettings: Dispatch<SetStateAction<IColumn[]>>,
         loadingSettings: Dispatch<SetStateAction<boolean>>
     ) => {
+        loadingSettings(true)//блокируем кнопку для нового массива
         sortingSettings === 'ascending'
-            ? setStateList({...stateList, isIncreasing: true})
-            : setStateList({...stateList, isDecreasing: true})
-        loadingSettings(true)
+            ? setStateList({...stateList, isIncreasing: true})//разворачиваем растущий стейт, блокируем кнопки
+            : setStateList({...stateList, isDecreasing: true})//в противном случае разворачиваем стейт на понижение, блокируем кнопки
 
-        //создаем копию массива
-        const array = [...initialArray];
-        array.forEach((element) => (element.state = ElementStates.Default))
+        const array = [...initialArray];//создаем копию массива
+        array.forEach((element) => (element.state = ElementStates.Default))//пробегаемся по всем элементам и задаем начальное состояние - синий цвет
 
         for (let i = 0; i < array.length - 1; i++) {
             let minIndex = i;//i - наименьший по индексу элемент
             array[minIndex].state = ElementStates.Changing;//визуализируем изменения
             for (let j = i + 1; j < array.length; j++) {
                 array[j].state = ElementStates.Changing;//визуализируем елемент
-                //отрисовываем
-                initialArraySettings([...array]);
-                await setDelay(SHORT_DELAY_IN_MS);
+                initialArraySettings([...array]);//отрисовываем
+                await setDelay(SHORT_DELAY_IN_MS);//устанавливаем задержку
                 //сравниваем
                 if ((sortingSettings === 'ascending' ? array[minIndex].number : array[j].number) > (sortingSettings === 'ascending' ? array[j].number : array[minIndex].number)) {
-                    //после сравнения найден второй элемент на замену, i - визуализируем по дефолту
+                    //после сравнения найден второй элемент на замену, i - визуализируем по дефолту синим
                     array[minIndex].state = i === minIndex ? ElementStates.Changing : ElementStates.Default;
                     minIndex = j;
-                    //отрисовываем
-                    initialArraySettings([...array]);
-                    await setDelay(SHORT_DELAY_IN_MS);
+                    initialArraySettings([...array]);//отрисовываем
+                    await setDelay(SHORT_DELAY_IN_MS);//устанавливаем задержку
                 }
                 //визуализируем элемент имеющий максимальное значение в массиве
                 if (j !== minIndex) {
-                    array[j].state = ElementStates.Modified;
-                    //отрисовываем
-                    initialArraySettings([...array]);
-                    await setDelay(SHORT_DELAY_IN_MS);
+                    array[j].state = ElementStates.Modified;//красим зеленым
+                    initialArraySettings([...array]);//отрисовываем
+                    await setDelay(SHORT_DELAY_IN_MS);//устанавливаем задержку
                 }
             }
             if (i === minIndex) {
-                array[i].state = ElementStates.Modified
-                //отрисовываем
-                initialArraySettings([...array]);
-                await setDelay(SHORT_DELAY_IN_MS);
+                array[i].state = ElementStates.Modified//красим зеленым
+                initialArraySettings([...array]);//отрисовываем
+                await setDelay(SHORT_DELAY_IN_MS);//устанавливаем задержку
             } else {//переставляем
                 swapArrayDetails(array, minIndex, i);
                 array[i].state = ElementStates.Modified
-                //отрисовываем
-                initialArraySettings([...array]);
-                await setDelay(SHORT_DELAY_IN_MS);
-                array[minIndex].state = ElementStates.Default
-                //отрисовываем
-                initialArraySettings([...array]);
-                await setDelay(SHORT_DELAY_IN_MS);
+                initialArraySettings([...array]);//отрисовываем
+                await setDelay(SHORT_DELAY_IN_MS);//устанавливаем задержку
+                array[minIndex].state = ElementStates.Default //возвращаем начальное состояние - синий
+                initialArraySettings([...array]);//отрисовываем
+                await setDelay(SHORT_DELAY_IN_MS);//устанавливаем задержку
             }
         }
-        array.forEach((element) => (element.state = ElementStates.Modified));
-        loadingSettings(false);
-        sortingSettings === 'ascending' ? setStateList({
-            ...stateList,
-            isIncreasing: false
-        }) : setStateList({...stateList, isDecreasing: false})
+        array.forEach((element) => (element.state = ElementStates.Modified));//пробегаемся по всем элементам и звзвращаем начальное состояние - синий цвет
+        loadingSettings(false);//разблокируем кнопку для нового массива
+        sortingSettings === 'ascending' ? setStateList({...stateList, isIncreasing: false}) : setStateList({...stateList, isDecreasing: false}) //разворачиваем стейт разблокируем кнопки
     }
 
     /**
@@ -132,47 +118,43 @@ export const SortingPage: React.FC = () => {
         initialArraySettings: Dispatch<SetStateAction<IColumn[]>>,
         loadingSettings: Dispatch<SetStateAction<boolean>>
     ) => {
+        loadingSettings(true);//блокируем кнопку для нового массива
         sortingSettings === 'ascending'
-            ? setStateList({...stateList, isIncreasing: true})
-            : setStateList({...stateList, isDecreasing: true})
-        loadingSettings(true);
+            ? setStateList({...stateList, isIncreasing: true})//разворачиваем растущий стейт, блокируем кнопки
+            : setStateList({...stateList, isDecreasing: true})//в противном случае разворачиваем стейт на понижение, блокируем кнопки
 
         //создаем копию массива
         const array = [...initialArray];
-        array.forEach((element) => (element.state = ElementStates.Default));
+        array.forEach((element) => (element.state = ElementStates.Default));//пробегаемся по всем элементам и звзвращаем начальное состояние - синий цвет
         for (let i = 0; i < array.length; i++) {
             for (let j = 0; j < array.length - i - 1; j++) {
                 //визуализируем сравниваемые элементы
-                array[j].state = ElementStates.Changing;
-                array[j + 1].state = ElementStates.Changing;
-                // отрисовываем
-                initialArraySettings([...array]);
-                await setDelay(SHORT_DELAY_IN_MS);
-
+                array[j].state = ElementStates.Changing;//красим розовым
+                array[j + 1].state = ElementStates.Changing;//красим розовым
+                initialArraySettings([...array]);//отрисовываем
+                await setDelay(SHORT_DELAY_IN_MS)//устанавливаем задержку
+                //сравниваем
                 if ((sortingSettings === 'ascending' ? array[j].number : array[j + 1].number) > (sortingSettings === 'ascending' ? array[j + 1].number : array[j].number)) {
-                    //меняем местами
-                    swapArrayDetails(array, j, j + 1);
-                    // отрисовываем
-                    initialArraySettings([...array]);
-                    await setDelay(SHORT_DELAY_IN_MS)
+                    swapArrayDetails(array, j, j + 1);//меняем местами
+                    initialArraySettings([...array]);// отрисовываем
+                    await setDelay(SHORT_DELAY_IN_MS)//устанавливаем задержку
                 }
-                //убираем визуализацию после сравнения
+                //возвращаем в начальное состояние после сравнения - синий цвет
                 array[j].state = ElementStates.Default;
                 array[j + 1].state = ElementStates.Default
                 //визуализируем отрисованный в цикле
                 if (j === array.length - i - 2) {
-                    array[j + 1].state = ElementStates.Modified;
+                    array[j + 1].state = ElementStates.Modified; //красим зеленым
                 }
-                // отрисовываем
-                initialArraySettings([...array]);
-                await setDelay(SHORT_DELAY_IN_MS);
+                initialArraySettings([...array]);// отрисовываем
+                await setDelay(SHORT_DELAY_IN_MS);//устанавливаем задержку
             }
         }
-        array.forEach((element) => (element.state = ElementStates.Modified))
-        loadingSettings(false);
+        array.forEach((element) => (element.state = ElementStates.Modified))//пробегаемся по всем элементам и красим в зеленый
+        loadingSettings(true);//разблокируем кнопку нового массива
         sortingSettings === 'ascending'
-            ? setStateList({...stateList, isIncreasing: false})
-            : setStateList({...stateList, isDecreasing: false})
+            ? setStateList({...stateList, isIncreasing: false})//разворачиваем растущий стейт, разблокируем кнопки
+            : setStateList({...stateList, isDecreasing: false})// в противном сулчае разворачиваем стейт на понижение разблокируем кнопки
     };
 
     return (
