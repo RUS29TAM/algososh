d9e757492cdddb79d0cd6341ab141cadaf05a7aa
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-05-05 10:13:43 +0300

    feature: add func `addHead`

--- src/pages/list-page/list-page.tsx
+++ src/pages/list-page/list-page.tsx
@@ -1,14 +1,18 @@
-import React, {FormEvent, useState} from "react";
+import React, {FormEvent, useEffect, useMemo, useState} from "react";
 import {SolutionLayout} from "../../components/ui/solution-layout/solution-layout";
 import style from "./list-page.module.css";
 import {Button} from "../../components/ui/button/button";
 import {Input} from "../../components/ui/input/input";
+import {randomChar} from "../../utils/random-char";
+import {LinkedList} from "./linked-list-node";
+import {ElementStates} from "../../types/element-states";
+import {setDelay} from "../../utils/set-delay";
+import {SHORT_DELAY_IN_MS} from "../../constants/delays";
 import {Circle} from "../../components/ui/circle/circle";
 import {ArrowIcon} from "../../components/ui/icons/arrow-icon";
 import {TLinkedListElement} from "../../types/t-linked-list-element";
 import {ICircleDetail} from "../../interfaces/i-circle-detail";
 
-//def
 export const ListPage: React.FC = () => {
         const [inputValue, setInputValue] = useState<string>('')
         const [inputValueIndex, setInputValueIndex] = useState<number>()
@@ -21,7 +25,34 @@ export const ListPage: React.FC = () => {
             isDeleteHead: false,
             isDeleteTail: false
         });
+        const maxLength = 12;
+        const initialArray = useMemo(() => Array.from({length: 6}, () => `${randomChar()}`), [])
+        const initialState: ICircleDetail[] = useMemo(() => [], [])
+        const linkedList = useMemo(() => new LinkedList<string>(initialArray), [initialArray])
 
+        useEffect(() => {
+            initialArray.forEach((element) => {
+                initialState.push({characters: element, state: ElementStates.Default});
+            });
+            setArrayCharacters(initialState.reverse());
+        }, [initialArray, initialState]);
+
+        const inLoading = useMemo<boolean>(() => !!Object.values(stateList).find((element) => element), [stateList])
+        const addHead = async () => {
+            setStateList({...stateList, isAddHead: true});//разворачиваем стейт блокируем кнопку
+            const array = [...arrayCharacters];//определяем масссив
+            linkedList.prepend(inputValue);//вставляем в начало элемента другой элемент.
+            array[0] = {...array[0], add: true, extra: {characters: linkedList.getNodeByIndex(0) || ''}}//определяем место для нового элемента в начале массива визуализируем изменения
+            setArrayCharacters([...array]);//разворачиваем массив с новым элементом
+            await setDelay(SHORT_DELAY_IN_MS)//устаначливаем задержку
+            array[0] = {...array[0], add: false, extra: undefined};//убираем визуализацию предыдущего действия
+            array.unshift({characters: linkedList.getNodeByIndex(0) || '', state: ElementStates.Modified})//добавляем элемент в начало массива визуализируем изменения
+            setArrayCharacters([...array])//разворачиваем массив с новым элементом
+            await setDelay(SHORT_DELAY_IN_MS)//устаначливаем задержку
+            array[0].state = ElementStates.Default;//возвращаем начальное визуальное состояние отображаемых элементов
+            setStateList({...stateList, isAddHead: false})//разворачиваем стейт разблокируем блокируем кнопку
+            setInputValue('')//очищаем инпут
+        }
         const onChange = (e: FormEvent<HTMLInputElement>) => {
             setInputValue(e.currentTarget.value)
         }
@@ -39,6 +70,7 @@ export const ListPage: React.FC = () => {
                                maxLength={4}
                                style={{width: 204, marginRight: 12}}
                                placeholder={'Введите  значение'}
+                               disabled={inLoading}
                                onChange={onChange}
                         >
                         </Input>
@@ -47,22 +79,21 @@ export const ListPage: React.FC = () => {
                                 <Button type={'button'}
                                         text={'Добавить в head'}
                                         style={{width: 175}}
+                                        onClick={() => addHead()}
                                         isLoader={stateList.isAddHead}
+                                        disabled={!inputValue || inLoading || arrayCharacters.length > maxLength}
                                 />
                                 <Button type={'button'}
                                         text={'Добавить в tail'}
                                         style={{width: 175}}
-                                        isLoader={stateList.isAddTail}
                                 />
                                 <Button type={'button'}
                                         text={'Удалить из head'}
                                         style={{width: 175}}
-                                        isLoader={stateList.isDeleteHead}
                                 />
                                 <Button type={'button'}
                                         text={'Удалить из tail'}
                                         style={{width: 175}}
-                                        isLoader={stateList.isDeleteTail}
                                 />
                             </div>
                         </section>
@@ -74,32 +105,52 @@ export const ListPage: React.FC = () => {
                                maxLength={1}
                                style={{width: 204, marginRight: 12}}
                                placeholder={'Введите  индекс'}
+                               disabled={inLoading}
                         >
                         </Input>
                         <section className={style.btn}>
                             <Button type={'button'}
                                     text={'Добавить по индексу'}
                                     style={{width: 362}}
-                                    isLoader={stateList.isAddByIndex}
                             />
                             <Button type={'button'}
                                     text={'Удалить по индексу'}
                                     style={{width: 362}}
-                                    isLoader={stateList.isDeleteByIndex}
                             />
                         </section>
                     </div>
                 </div>
                 <ol className={style.elements}>
-
-                    <li key={'index'} className={style.elementsWrapper}>
-                        <Circle/>
-                        <ArrowIcon/>
-                        <Circle isSmall={true}
-                                extraClass={style.top}/>
-                        <Circle isSmall={true}
-                                extraClass={style.bottom}/>
-                    </li>
+                    {arrayCharacters.map((characters, index) => {
+                        return (
+                            <li key={index} className={style.elementsWrapper}>
+                                <Circle letter={characters.characters}
+                                        index={index}
+                                        state={characters.state}
+                                        head={index === 0 && !characters.add && !characters.delete ? 'head' : ''}
+                                        tail={index === arrayCharacters.length - 1 && !characters.add && !characters.delete ? 'tail' : ''}
+                                />
+                                {index !== arrayCharacters.length - 1 && (
+                                    <ArrowIcon
+                                        fill={characters.state === ElementStates.Changing && !characters.arrow ? '#d252e1' : '#0032FF'}/>
+                                )}
+                                {characters.add && (
+                                    <Circle letter={characters.extra?.characters}
+                                            state={ElementStates.Changing}
+                                            isSmall={true}
+                                            extraClass={style.top}
+                                    />
+                                )}
+                                {characters.delete && (
+                                    <Circle letter={characters.extra?.characters}
+                                            state={ElementStates.Changing}
+                                            isSmall={true}
+                                            extraClass={style.bottom}
+                                    />
+                                )}
+                            </li>
+                        )
+                    })}
                 </ol>
             </SolutionLayout>
         );
