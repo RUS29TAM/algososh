63d948b0fdd7b513bbb47a8becb9dd5d5c0d9dd8
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-05-05 09:36:23 +0300

    chore: add tail, delete, arrow, extra & moving interface ICircleDetail in directory interfaces

--- src/interfaces/i-circle-detail.ts
+++ src/interfaces/i-circle-detail.ts
@@ -1,14 +1,12 @@
-import {ElementStates} from "./element-states";
-
+import {ElementStates} from "../types/element-states";
 export interface ICircleDetail {
     symbol?: null | string;
     state: ElementStates;
     head?: string;
     characters?: string | null;
     tail?: string;
-}
-
-export interface IColumn {
-    number: number;
-    state: ElementStates;
+    add?: boolean;
+    delete?: boolean;
+    arrow?: boolean;
+    extra?: { characters: string }
 }
