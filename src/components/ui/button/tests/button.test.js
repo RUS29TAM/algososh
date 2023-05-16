fb66e22f8122ace74fde534aaf2aa122a8734c31
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-05-16 17:53:55 +0300

    chore: `draft - test Button component`

--- src/components/ui/button/tests/button.test.js
+++ src/components/ui/button/tests/button.test.js
@@ -3,14 +3,39 @@ import {render, screen, fireEvent} from "@testing-library/react";
 import {Button} from "../button";
 
 describe('Тест для компонента Button', () => {
-    it('Компонент Button отрисовка текста компонента - тест пройден', () => {
+    it('Компонент Button отрисовка компонента с текстом - тест пройден', () => {
         const button = renderer.create(<Button text={'text'}/>).toJSON();
+        expect(button).toMatchSnapshot();
+    })
+
+    it('Компонент Button отрисовка компонента без текста - тест пройден', () => {
+        const button = renderer.create(<Button text={''}/>).toJSON();
+        expect(button).toMatchSnapshot();
+    })
+
+    it('Компонент Button в не активеном состоянии - тест пройден', () => {
+        const button = renderer.create(<Button disabled />).toJSON();
+        expect(button).toMatchSnapshot();
+    })
+
+    it('Компонент Button в не активеном состоянии - тест пройден', () => {
+        const button = renderer.create(<Button isLoader />).toJSON();
+        expect(button).toMatchSnapshot();
+    })
+
+    it('Компонент Button type "button" - тест пройден', () => {
+        const button = renderer.create(<Button type={'button'} />).toJSON();
+        expect(button).toMatchSnapshot();
+    })
+
+    it('Компонент Button type "submit" - тест пройден', () => {
+        const button = renderer.create(<Button type={'submit'} />).toJSON();
         expect(button).toMatchSnapshot();
     })
 })
 
 describe('Тест для слушателей событий компонента Button', () => {
-    it('Нажатие на кнопку работает - тест пройден', () => {
+    it('Нажатие на кнопку работает, колбэк вызывается корректно - тест пройден', () => {
         const onClick = jest.fn();
         render(<Button text={'text'} onClick={onClick}/>)
         const button = screen.getByText('text');
