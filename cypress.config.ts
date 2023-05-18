38e5c9c6d8ee1d668099f39e2e1d99d4e2cbf858
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-05-18 15:31:54 +0300

    chore: add `baseUrl: 'http://localhost:3000/'` in defineConfig

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from "cypress";
+import {defineConfig} from "cypress";
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        baseUrl: 'http://localhost:3000/',
+        viewportHeight: 1000,
+        viewportWidth: 1800,
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
 
-  component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
+    component: {
+        devServer: {
+            framework: "create-react-app",
+            bundler: "webpack",
+        },
     },
-  },
 });
