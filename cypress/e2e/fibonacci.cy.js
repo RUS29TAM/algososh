5b67aac62802b5d3ae9165ba957260d7ed9e751a
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-05-22 17:44:44 +0300

    e2e tests: `FibonacciComponent - tests complete`

--- cypress/e2e/fibonacci.cy.js
+++ cypress/e2e/fibonacci.cy.js
@@ -1,35 +1,63 @@
 import {circle} from "../support/constants";
+import {SHORT_DELAY_IN_MS} from "../../src/constants/delays";
 
 describe('Тест для компонента "Фибоначчи"', () => {
     beforeEach(() => {
-        cy.visit('/fibonacci')
+        cy.visit('/fibonacci');
     })
     describe('Состояние кнопок компонента', () => {
         it('Кнопка не активна при пустом инпуте - тест пройден', () => {
             cy.get('input[placeholder=\"Введите число\"]').should('have.value', '');
             cy.contains('Рассчитать').as('button');
-            cy.get('@button').should('be.disabled')
+            cy.get('@button').should('be.disabled');
         })
 
         it('Кнопка активна в инпут не пустой - тест пройден', () => {
             cy.get('input[placeholder=\"Введите число\"]').type('9');
             cy.contains('Рассчитать').as('button');
-            cy.get('@button').should('not.be.disabled')
+            cy.get('@button').should('not.be.disabled');
         })
 
         it('Кнопка не активна если инпут не валиден > 19 - тест пройден', () => {
             cy.get('input[placeholder=\"Введите число\"]').should('have.value', '').type('20');
             cy.contains('Рассчитать').as('button');
-            cy.get('@button').should('be.disabled')
+            cy.get('@button').should('be.disabled');
         })
     })
 
     describe('Тестирование алгоритма Фибоначчи', () => {
-        it('Строка расширяется корректно', () => {
+        it('Последовательность чисел расширяется корректно', () => {
             cy.get('input[placeholder=\"Введите число\"]').type('5');
             cy.contains('Рассчитать').as('button').click();
-            cy.get(circle).should('have.length', 1).each(($item, index) => {})
+            cy.get(circle).should('have.length', 1).each(($item, index) => {
+                const expectedValues = ['1'];
+                expect($item).to.contain(expectedValues[index]);
+            });
+            cy.wait(SHORT_DELAY_IN_MS)
+
+            cy.get(circle).should('have.length', 2).each(($item, index) => {
+                const expectedValues = ['1', '1'];
+                expect($item).to.contain(expectedValues[index]);
+            });
+            cy.wait(SHORT_DELAY_IN_MS)
+
+            cy.get(circle).should('have.length', 3).each(($item, index) => {
+                const expectedValues = ['1', '1', '2'];
+                expect($item).to.contain(expectedValues[index]);
+            });
+            cy.wait(SHORT_DELAY_IN_MS)
+
+            cy.get(circle).should('have.length', 4).each(($item, index) => {
+                const expectedValues = ['1', '1', '2', '3'];
+                expect($item).to.contain(expectedValues[index]);
+            });
+            cy.wait(SHORT_DELAY_IN_MS)
+
+            cy.get(circle).should('have.length', 5).each(($item, index) => {
+                const expectedValues = ['1', '1', '2', '3', '5'];
+                expect($item).to.contain(expectedValues[index]);
+            });
         });
-    })
-})
+    });
+});
 
