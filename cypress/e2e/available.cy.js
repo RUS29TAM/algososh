da0ea950ab77427ae8ce4c5fcf61054fdfa9c207
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-05-22 17:53:31 +0300

    chore: supplement the values of constants

--- cypress/e2e/available.cy.js
+++ cypress/e2e/available.cy.js
@@ -1,13 +1,15 @@
+import {fibonacci, list, queue, recursion, sorting, stack} from "../support/constants";
+
 describe('Запуск приложения', () => {
     it('Отрисовка главной страницы - тест пройден', () => {
         cy.visit('/')
         cy.contains('МБОУ АЛГОСОШ')
-        cy.get('a[href*="/recursion"]')
-        cy.get('a[href*="/fibonacci"]')
-        cy.get('a[href*="/sorting"]')
-        cy.get('a[href*="/stack"]')
-        cy.get('a[href*="/queue"]')
-        cy.get('a[href*="/list"]')
+        cy.get(recursion)
+        cy.get(fibonacci)
+        cy.get(sorting)
+        cy.get(stack)
+        cy.get(queue)
+        cy.get(list)
         cy.contains('Вдохновлено школами, в которых не учили алгоритмам')
         cy.contains('© RUS29TAM Сделано в Практикуме.')
     })
--- cypress/support/constants.js
+++ cypress/support/constants.js
@@ -1,1 +1,8 @@
+export const recursion = 'a[href*="/recursion"]';
+export const fibonacci = 'a[href*="/fibonacci"]';
+export const sorting = 'a[href*="/sorting"]';
+export const stack = 'a[href*="/stack"]';
+export const queue = 'a[href*="/queue"]';
+export const list = 'a[href*="/list"]'
+
 export const circle = '[class*=circle_circle]';
