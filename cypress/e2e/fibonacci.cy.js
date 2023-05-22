264a49a28bae8d009e1ae28d7827d1737225babc
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-05-22 17:25:07 +0300

    chore: correct placeholder value

--- cypress/e2e/fibonacci.cy.js
+++ cypress/e2e/fibonacci.cy.js
@@ -1,25 +1,35 @@
+import {circle} from "../support/constants";
+
 describe('Тест для компонента "Фибоначчи"', () => {
     beforeEach(() => {
         cy.visit('/fibonacci')
     })
     describe('Состояние кнопок компонента', () => {
         it('Кнопка не активна при пустом инпуте - тест пройден', () => {
-            cy.get('input[placeholder=\"Введите число от 1 до 19 (включительно)\"]').should('have.value', '');
+            cy.get('input[placeholder=\"Введите число\"]').should('have.value', '');
             cy.contains('Рассчитать').as('button');
             cy.get('@button').should('be.disabled')
         })
 
         it('Кнопка активна в инпут не пустой - тест пройден', () => {
-            cy.get('input[placeholder=\"Введите число от 1 до 19 (включительно)\"]').type('9');
+            cy.get('input[placeholder=\"Введите число\"]').type('9');
             cy.contains('Рассчитать').as('button');
             cy.get('@button').should('not.be.disabled')
         })
 
         it('Кнопка не активна если инпут не валиден > 19 - тест пройден', () => {
-            cy.get('input[placeholder=\"Введите число от 1 до 19 (включительно)\"]').should('have.value', '').type('20');
+            cy.get('input[placeholder=\"Введите число\"]').should('have.value', '').type('20');
             cy.contains('Рассчитать').as('button');
             cy.get('@button').should('be.disabled')
         })
+    })
+
+    describe('Тестирование алгоритма Фибоначчи', () => {
+        it('Строка расширяется корректно', () => {
+            cy.get('input[placeholder=\"Введите число\"]').type('5');
+            cy.contains('Рассчитать').as('button').click();
+            cy.get(circle).should('have.length', 1).each(($item, index) => {})
+        });
     })
 })
 
