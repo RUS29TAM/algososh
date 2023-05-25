6d5f34f4eafbaadf56424833f050ec706e849c0e
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-05-25 16:20:28 +0300

    e2e tests: `ListComponent - tests complete`

--- cypress/e2e/list.spec.cy.js
+++ cypress/e2e/list.spec.cy.js
@@ -1,177 +1,174 @@
 import {circle, circleContent, circleSmall, inputEnterIndex, inputEnterValue} from "../support/constants";
 import {SHORT_DELAY_IN_MS} from "../../src/constants/delays";
 
-describe('Тест для компонента "Очередь"', () => {
+describe('Тест для компонента "Связанный список"', () => {
     beforeEach(() => {
         cy.visit('/list')
     });
 
-    // describe('Состояние кнопок компонента "Связанный список"', () => {
-    //     it('Поле для ввода значения пустое, все кнопки не активны кроме "Удалить из head" "Удалить из tail"', () => {
-    //         cy.get(inputEnterValue).should('have.value', '');
-    //         cy.contains('Удалить из head').should('not.be.disabled')
-    //         cy.contains('Удалить из tail').should('not.be.disabled')
-    //         cy.contains('Добавить в head').should('be.disabled')
-    //         cy.contains('Добавить в tail').should('be.disabled')
-    //         cy.contains('Добавить в tail').should('be.disabled')
-    //         cy.contains('Добавить по индексу').should('be.disabled')
-    //         cy.contains('Удалить по индексу').should('be.disabled')
-    //     })
-    //
-    //     it('Поле для ввода значения не пустое, все кнопки не активны кроме "Удалить из head" "Удалить из tail"', () => {
-    //         cy.get(inputEnterValue).type('PAIN');
-    //         cy.contains('Удалить из head').should('not.be.disabled')
-    //         cy.contains('Удалить из tail').should('not.be.disabled')
-    //         cy.contains('Добавить в head').should('not.be.disabled')
-    //         cy.contains('Добавить в tail').should('not.be.disabled')
-    //         cy.contains('Добавить в tail').should('not.be.disabled')
-    //         cy.contains('Добавить по индексу').should('be.disabled')
-    //         cy.contains('Удалить по индексу').should('be.disabled')
-    //     })
-    //
-    //     it('Когда оба инпута заполнены данными - все кнопки активны', () => {
-    //         cy.get(inputEnterValue).type('PAIN');
-    //         cy.get(inputEnterIndex).type('5');
-    //         cy.contains('Удалить из head').should('not.be.disabled')
-    //         cy.contains('Удалить из tail').should('not.be.disabled')
-    //         cy.contains('Добавить в head').should('not.be.disabled')
-    //         cy.contains('Добавить в tail').should('not.be.disabled')
-    //         cy.contains('Добавить в tail').should('not.be.disabled')
-    //         cy.contains('Добавить по индексу').should('not.be.disabled')
-    //         cy.contains('Удалить по индексу').should('not.be.disabled')
-    //     })
-    // })
+    describe('Состояние кнопок компонента "Связанный список"', () => {
+        it('Поле для ввода значения пустое, все кнопки не активны кроме "Удалить из head" "Удалить из tail"', () => {
+            cy.get(inputEnterValue).should('have.value', '');
+            cy.contains('Удалить из head').should('not.be.disabled')
+            cy.contains('Удалить из tail').should('not.be.disabled')
+            cy.contains('Добавить в head').should('be.disabled')
+            cy.contains('Добавить в tail').should('be.disabled')
+            cy.contains('Добавить в tail').should('be.disabled')
+            cy.contains('Добавить по индексу').should('be.disabled')
+            cy.contains('Удалить по индексу').should('be.disabled')
+        })
+
+        it('Поле для ввода значения не пустое, все кнопки не активны кроме "Удалить из head" "Удалить из tail"', () => {
+            cy.get(inputEnterValue).type('PAIN');
+            cy.contains('Удалить из head').should('not.be.disabled')
+            cy.contains('Удалить из tail').should('not.be.disabled')
+            cy.contains('Добавить в head').should('not.be.disabled')
+            cy.contains('Добавить в tail').should('not.be.disabled')
+            cy.contains('Добавить в tail').should('not.be.disabled')
+            cy.contains('Добавить по индексу').should('be.disabled')
+            cy.contains('Удалить по индексу').should('be.disabled')
+        })
+
+        it('Когда оба инпута заполнены данными - все кнопки активны', () => {
+            cy.get(inputEnterValue).type('PAIN');
+            cy.get(inputEnterIndex).type('5');
+            cy.contains('Удалить из head').should('not.be.disabled')
+            cy.contains('Удалить из tail').should('not.be.disabled')
+            cy.contains('Добавить в head').should('not.be.disabled')
+            cy.contains('Добавить в tail').should('not.be.disabled')
+            cy.contains('Добавить в tail').should('not.be.disabled')
+            cy.contains('Добавить по индексу').should('not.be.disabled')
+            cy.contains('Удалить по индексу').should('not.be.disabled')
+        })
+    })
 
     describe('Тестирование алгоритма "Связанный список"', () => {
-        // it('Элементы связанного списка по умолчанию отрисованы корректно', () => {
-        //     cy.get(circleContent)
-        //         .should('have.length', 6)
-        //         .each(($item, index) => {
-        //             const circleCircle = cy.wrap($item).find(circle);
-        //             circleCircle.should('have.css', 'border', '4px solid rgb(0, 50, 255)');
-        //             if (index === 0) expect($item).to.contain('head');
-        //             if (index === 5) expect($item).to.contain('tail');
-        //         });
-        // });
-        //
-        // it('Добавить ноду в начало связанного списка, рендер и визуализация корректно отработаны', () => {
-        //     const inputValue = 'PAIN';
-        //     cy.get(inputEnterValue).type(inputValue);
-        //     cy.contains('Добавить в head').click();
-        //
-        //     cy.get(circleSmall)
-        //         .eq(0)
-        //         .should('have.css', 'border', '4px solid rgb(210, 82, 225)')
-        //         .should('contain', inputValue);
-        //     cy.wait(SHORT_DELAY_IN_MS)
-        //     cy.get(circleContent).first().should('contain', inputValue);
-        //     cy.get(circle)
-        //         .eq(0)
-        //         .should('have.css', 'border', '4px solid rgb(127, 224, 81)')
-        //         .should('contain', inputValue);
-        //     cy.wait(SHORT_DELAY_IN_MS)
-        //     cy.get(circle)
-        //         .eq(0)
-        //         .should('have.css', 'border', '4px solid rgb(0, 50, 255)')
-        //         .should('contain', inputValue);
-        // });
-//отмена
-        //отмена
+        it('Элементы связанного списка по умолчанию отрисованы корректно', () => {
+            cy.get(circleContent)
+                .should('have.length', 6)
+                .each(($item, index) => {
+                    const circleCircle = cy.wrap($item).find(circle);
+                    circleCircle.should('have.css', 'border', '4px solid rgb(0, 50, 255)');
+                    if (index === 0) expect($item).to.contain('head');
+                    if (index === 5) expect($item).to.contain('tail');
+                });
+        });
 
-        // it('Добавить ноду в конец связанного списка, рендер и визуализация корректно отработаны', () => {
-        //     const inputValue = 'PAIN';
-        //     cy.get(inputEnterValue).type(inputValue);
-        //     cy.contains('Добавить в tail').click();
-        //     const tailText = 'tail';
-        //     const headText = 'head'
-        //
-        //     for (let i = 0; i <= 6; i++) {
-        //         cy.get(circleContent).each(($item, index) => {
-        //             if (index < i) {
-        //                 cy.wrap($item).find(circle)
-        //                     .should('have.css', 'border', '4px solid rgb(210, 82, 225)');
-        //             } else if (index === i) {
-        //                 cy.wrap($item).siblings().find(circleSmall)
-        //                     .should('have.css', 'border', '4px solid rgb(210, 82, 225)')
-        //                     .should('have.text', inputValue);
-        //             }
-        //         });
-        //         cy.wait(SHORT_DELAY_IN_MS);
-        //     }
-        //
-        //     cy.get(circleContent).last().find(circle)
-        //         .should('have.css', 'border', '4px solid rgb(127, 224, 81)')
-        //         .should('have.text', inputValue)
-        //     cy.wait(SHORT_DELAY_IN_MS);
-        //
-        //     cy.get(circleContent).each(($item) => {
-        //         cy.wrap($item).find(circle)
-        //             .should('have.css', 'border', '4px solid rgb(0, 50, 255)')
-        //         cy.get(circleContent).last().should('contain.text', tailText)
-        //         cy.get(circleContent).first().should('contain.text', headText)
-        //     });
-        // });
+        it('Добавить ноду в начало связанного списка, рендер и визуализация корректно отработаны', () => {
+            const inputValue = 'PAIN';
+            cy.get(inputEnterValue).type(inputValue);
+            cy.contains('Добавить в head').click();
 
-        // it('Добавить ноду в список по индексу, рендер и визуализация корректно отработаны', () => {
-        //     const inputValue = 'MAX';
-        //     const inputIndex = '5'
-        //     const tailText = 'tail';
-        //     const headText = 'head'
-        //     cy.get(inputEnterValue).type(inputValue);
-        //     cy.get(inputEnterIndex).type(inputIndex);
-        //     cy.contains('Добавить по индексу').click();
-        //
-        //     for (let i = 0; i <= 5; i++) {
-        //         cy.get(circleContent).each(($item, index) => {
-        //
-        //             if (index < i) {
-        //                 cy.wrap($item).find(circle)
-        //                     .should('have.css', 'border', '4px solid rgb(210, 82, 225)');
-        //             } else if (index === i) {
-        //                 cy.wrap($item).siblings().find(circleSmall)
-        //                     .should('have.css', 'border', '4px solid rgb(210, 82, 225)')
-        //                     .should('have.text', inputValue);
-        //             }
-        //         });
-        //         cy.wait(SHORT_DELAY_IN_MS);
-        //     }
-        //
-        //     cy.get(circleContent).eq(5).find(circle)
-        //         .should('have.css', 'border', '4px solid rgb(127, 224, 81)')
-        //         .should('have.text', inputValue);
-        //     cy.wait(SHORT_DELAY_IN_MS);
-        //
-        //     cy.get(circleContent).eq(5).find(circle)
-        //         .should('have.css', 'border', '4px solid rgb(0, 50, 255)')
-        //         .should('have.text', inputValue);
-        //
-        //     cy.get(circleContent).last().should('contain.text', tailText)
-        //     cy.get(circleContent).first().should('contain.text', headText)
-        // })
+            cy.get(circleSmall)
+                .eq(0)
+                .should('have.css', 'border', '4px solid rgb(210, 82, 225)')
+                .should('contain', inputValue);
+            cy.wait(SHORT_DELAY_IN_MS)
+            cy.get(circleContent).first().should('contain', inputValue);
+            cy.get(circle)
+                .eq(0)
+                .should('have.css', 'border', '4px solid rgb(127, 224, 81)')
+                .should('contain', inputValue);
+            cy.wait(SHORT_DELAY_IN_MS)
+            cy.get(circle)
+                .eq(0)
+                .should('have.css', 'border', '4px solid rgb(0, 50, 255)')
+                .should('contain', inputValue);
+        });
 
-        // it('Удалить ноду из списка по индексу, рендер и визуализация корректно отработаны', () => {
-        //         const inputIndex = '5'
-        //         const tailText = 'tail';
-        //         const headText = 'head'
-        //         cy.get(inputEnterIndex).type(inputIndex);
-        //         cy.contains('Удалить по индексу').click();
-        //
-        //     for (let i = 0; i <= 5; i++) {
-        //         cy.get(circleContent).each(($item, index) => {
-        //             if (index <= i)
-        //                 cy.wrap($item).find(circle).should('have.css', 'border', '4px solid rgb(210, 82, 225)');
-        //         });
-        //         cy.wait(SHORT_DELAY_IN_MS);
-        //     }
-        //     cy.get(circleContent).eq(6).find(circleSmall)
-        //         .should('have.css', 'border', '4px solid rgb(210, 82, 225)')
-        //         .should('not.have.text', '');
-        //     cy.wait(SHORT_DELAY_IN_MS);
-        //
-        //     cy.get(circle).should('have.css', 'border', '4px solid rgb(0, 50, 255)');
-        //     cy.get(circleContent).last().should('contain.text', tailText)
-        //     cy.get(circleContent).first().should('contain.text', headText)
-        // })
+        it('Добавить ноду в конец связанного списка, рендер и визуализация корректно отработаны', () => {
+            const inputValue = 'PAIN';
+            cy.get(inputEnterValue).type(inputValue);
+            cy.contains('Добавить в tail').click();
+            const tailText = 'tail';
+            const headText = 'head'
+
+            for (let i = 0; i <= 6; i++) {
+                cy.get(circleContent).each(($item, index) => {
+                    if (index < i) {
+                        cy.wrap($item).find(circle)
+                            .should('have.css', 'border', '4px solid rgb(210, 82, 225)');
+                    } else if (index === i) {
+                        cy.wrap($item).siblings().find(circleSmall)
+                            .should('have.css', 'border', '4px solid rgb(210, 82, 225)')
+                            .should('have.text', inputValue);
+                    }
+                });
+                cy.wait(SHORT_DELAY_IN_MS);
+            }
+
+            cy.get(circleContent).last().find(circle)
+                .should('have.css', 'border', '4px solid rgb(127, 224, 81)')
+                .should('have.text', inputValue)
+            cy.wait(SHORT_DELAY_IN_MS);
+
+            cy.get(circleContent).each(($item) => {
+                cy.wrap($item).find(circle)
+                    .should('have.css', 'border', '4px solid rgb(0, 50, 255)')
+                cy.get(circleContent).last().should('contain.text', tailText)
+                cy.get(circleContent).first().should('contain.text', headText)
+            });
+        });
+
+        it('Добавить ноду в список по индексу, рендер и визуализация корректно отработаны', () => {
+            const inputValue = 'MAX';
+            const inputIndex = '5'
+            const tailText = 'tail';
+            const headText = 'head'
+            cy.get(inputEnterValue).type(inputValue);
+            cy.get(inputEnterIndex).type(inputIndex);
+            cy.contains('Добавить по индексу').click();
+
+            for (let i = 0; i <= 5; i++) {
+                cy.get(circleContent).each(($item, index) => {
+                    if (index < i) {
+                        cy.wrap($item).find(circle)
+                            .should('have.css', 'border', '4px solid rgb(210, 82, 225)');
+                    } else if (index === i) {
+                        cy.wrap($item).siblings().find(circleSmall)
+                            .should('have.css', 'border', '4px solid rgb(210, 82, 225)')
+                            .should('have.text', inputValue);
+                    }
+                });
+                cy.wait(SHORT_DELAY_IN_MS);
+            }
+
+            cy.get(circleContent).eq(5).find(circle)
+                .should('have.css', 'border', '4px solid rgb(127, 224, 81)')
+                .should('have.text', inputValue);
+            cy.wait(SHORT_DELAY_IN_MS);
+
+            cy.get(circleContent).eq(5).find(circle)
+                .should('have.css', 'border', '4px solid rgb(0, 50, 255)')
+                .should('have.text', inputValue);
+
+            cy.get(circleContent).last().should('contain.text', tailText)
+            cy.get(circleContent).first().should('contain.text', headText)
+        })
+
+        it('Удалить ноду из списка по индексу, рендер и визуализация корректно отработаны', () => {
+            const inputIndex = '5'
+            const tailText = 'tail';
+            const headText = 'head'
+            cy.get(inputEnterIndex).type(inputIndex);
+            cy.contains('Удалить по индексу').click();
+
+            for (let i = 0; i <= 5; i++) {
+                cy.get(circleContent).each(($item, index) => {
+                    if (index <= i)
+                        cy.wrap($item).find(circle).should('have.css', 'border', '4px solid rgb(210, 82, 225)');
+                });
+                cy.wait(SHORT_DELAY_IN_MS);
+            }
+            cy.get(circleContent).eq(6).find(circleSmall)
+                .should('have.css', 'border', '4px solid rgb(210, 82, 225)')
+                .should('not.have.text', '');
+            cy.wait(SHORT_DELAY_IN_MS);
+
+            cy.get(circle).should('have.css', 'border', '4px solid rgb(0, 50, 255)');
+            cy.get(circleContent).last().should('contain.text', tailText)
+            cy.get(circleContent).first().should('contain.text', headText)
+        })
 
         it('Удалить ноду из head, рендер и визуализация корректно отработаны', () => {
             const tailText = 'tail';
@@ -191,6 +188,27 @@ describe('Тест для компонента "Очередь"', () => {
                 .should('have.css', 'border', '4px solid rgb(0, 50, 255)');
 
             cy.get(circleContent).last().should('contain.text', tailText);
+            cy.get(circleContent).first().should('contain.text', headText);
+        })
+
+        it('Удалить ноду из tail, рендер и визуализация корректно отработаны', () => {
+            const tailText = 'tail';
+            const headText = 'head'
+            cy.contains('Удалить из tail').click();
+
+            cy.get(circleContent).eq(5).siblings().find(circleSmall)
+                .should('have.css', 'border', '4px solid rgb(210, 82, 225)')
+                .should('not.have.text', '');
+            cy.wait(SHORT_DELAY_IN_MS);
+
+            cy.get(circleContent).last().find(circle)
+                .should('have.css', 'border', '4px solid rgb(127, 224, 81)');
+            cy.wait(SHORT_DELAY_IN_MS);
+
+            cy.get(circleContent).last()
+                .should('contain', tailText)
+                .find(circle)
+                .should('have.css', 'border', '4px solid rgb(0, 50, 255)');
             cy.get(circleContent).first().should('contain.text', headText);
         })
     });
