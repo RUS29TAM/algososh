cc8cd549c049d9549a0084979896563c080e208f
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-05-27 01:00:07 +0300

    chore: `remove redundant text in the test description that may contradict the actual state after the test`

--- src/components/ui/circle/tests/circle.test.js
+++ src/components/ui/circle/tests/circle.test.js
@@ -3,57 +3,57 @@ import {Circle} from "../circle";
 import {ElementStates} from "../../../../types/element-states";
 
 describe('Тест для компонента Circle', () =>{
-    it('Компонент Circle отрисовка компонента с символами - тест пройден', () => {
+    it('Компонент Circle отрисовка компонента с символами', () => {
         const circle = renderer.create(<Circle letter={'symbol'}/>).toJSON();
         expect(circle).toMatchSnapshot();
     })
 
-    it('Компонент Circle отрисовка без символов - тест пройден', () => {
+    it('Компонент Circle отрисовка без символов', () => {
         const circle = renderer.create(<Circle letter={''}/>).toJSON();
         expect(circle).toMatchSnapshot();
     })
 
-    it('Компонент Circle отрисовка head - тест пройден', () => {
+    it('Компонент Circle отрисовка head', () => {
         const circle = renderer.create(<Circle head={'string'}/>).toJSON();
         expect(circle).toMatchSnapshot();
     })
 
-    it('Компонент Circle отрисовка tail - тест пройден', () => {
+    it('Компонент Circle отрисовка tail', () => {
         const circle = renderer.create(<Circle tail={'string'}/>).toJSON();
         expect(circle).toMatchSnapshot();
     })
 
-    it('Компонент Circle отрисовка head c react-element - тест пройден', () => {
+    it('Компонент Circle отрисовка head c react-element', () => {
         const circle = renderer.create(<Circle head={<Circle isSmall={true}/>} />).toJSON();
         expect(circle).toMatchSnapshot();
     })
 
-    it('Компонент Circle отрисовка tail c react-element - тест пройден', () => {
+    it('Компонент Circle отрисовка tail c react-element', () => {
         const circle = renderer.create(<Circle tail={<Circle isSmall={true}/>} />).toJSON();
         expect(circle).toMatchSnapshot();
     })
 
-    it('Компонент Circle отрисовка по индексу - тест пройден', () => {
+    it('Компонент Circle отрисовка по индексу', () => {
         const circle = renderer.create(<Circle index={0}/>).toJSON();
         expect(circle).toMatchSnapshot();
     })
 
-    it('Компонент Circle отрисовка с реквизитом isSmall - тест пройден', () => {
+    it('Компонент Circle отрисовка с реквизитом isSmall', () => {
         const circle = renderer.create(<Circle isSmall={true}/>).toJSON();
         expect(circle).toMatchSnapshot();
     })
 
-    it('Компонент Circle отрисовка в состоянии Default - тест пройден', () => {
+    it('Компонент Circle отрисовка в состоянии Default', () => {
         const circle = renderer.create(<Circle state={ElementStates.Default}/>).toJSON();
         expect(circle).toMatchSnapshot();
     })
 
-    it('Компонент Circle отрисовка в состоянии Changing - тест пройден', () => {
+    it('Компонент Circle отрисовка в состоянии Changing', () => {
         const circle = renderer.create(<Circle state={ElementStates.Changing}/>).toJSON();
         expect(circle).toMatchSnapshot();
     })
 
-    it('Компонент Circle отрисовка в состоянии Modified - тест пройден', () => {
+    it('Компонент Circle отрисовка в состоянии Modified', () => {
         const circle = renderer.create(<Circle state={ElementStates.Modified}/>).toJSON();
         expect(circle).toMatchSnapshot();
     })
